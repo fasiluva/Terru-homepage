bafa16f96f3cc7abb68d38a19f095158f5bd3e54
fasiluva <valentinSosa1210@gmail.com>
2026-01-13 12:44:16 -0300

    Minor changes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import React from "react"
 
 const Header = () => {
     return (
-        <header className="m-0 bg-cover flex justify-center items-center flex-col h-banner-height bg-[url('../public/terrunio.jpeg')]">
-            <img 
+        <header className="m-0 bg-cover flex justify-center items-center flex-col h-banner-height bg-[url('/terrunio.jpeg')]">
+        <img 
             src="/terruSinFondo2.png" 
             alt="" 
             className="w-[80%] sm:w-[50%] md:w-[40%] max-h-full max-w-full object-contain"
-            />
-        </header>
+        />
+</header>
+
     )
 }
 
